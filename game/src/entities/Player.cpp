bc4b445de0e81239dcb2f6aaa379b03ceaca23f5
lackle1 <lachlankeane014@gmail.com>
2025-08-27 16:25:34 +1000

    Fixed diagonal movement

--- game/src/entities/Player.cpp
+++ game/src/entities/Player.cpp
@@ -3,7 +3,7 @@
 Player::Player() : Engine::Entity("Player"),
 	m_deceleration(20.f),
 	m_maxSpeed(250.f),
-	m_acceleration(1000.f),
+	m_acceleration(3000.f),
 	health(10)
 {
 	GetTransform()->scale = { 32.f, 32.f };
@@ -14,7 +14,7 @@ void Player::OnUpdate(Engine::Timestep ts) {
 	// Movement
 
 	// Currently confined to what the camera sees
-	glm::vec3 direction = glm::vec3(0.f);
+	glm::vec2 direction = glm::vec2(0.f);
 	bool hitWall = false;
 	if (Engine::Input::IsKeyPressed(EG_KEY_D)) {
 		direction.x = 1;
@@ -46,29 +46,31 @@ void Player::OnUpdate(Engine::Timestep ts) {
 			hitWall = true;
 		}
 	}
-	if ((Engine::Input::IsKeyPressed(EG_KEY_W) && GetVelocity()->velocity.y < 0) || (Engine::Input::IsKeyPressed(EG_KEY_S) && GetVelocity()->velocity.y > 0)) {
-		GetVelocity()->velocity.y = 0;
-	}
+	//if ((Engine::Input::IsKeyPressed(EG_KEY_W) && GetVelocity()->velocity.y < 0) || (Engine::Input::IsKeyPressed(EG_KEY_S) && GetVelocity()->velocity.y > 0)) {
+	//	GetVelocity()->velocity.y = 0;
+	//}
 
-	if (glm::length(direction) != 0 && glm::length(GetVelocity()->velocity) < m_maxSpeed) {
-		// v = v + direction.nor * a
-		// Got to take the normal of the direction so that player speed is calculated correctly on diagonals
-		GetVelocity()->velocity += glm::normalize(direction) * m_acceleration * ts.GetSeconds();
-	} else {
-		// v = v + -v * deceleration
-		GetVelocity()->velocity += -GetVelocity()->velocity * m_deceleration * ts.GetSeconds();
-		if (glm::length(GetVelocity()->velocity) < 20.f)
-			GetVelocity()->velocity = { 0.f, 0.f, 0.f };
-	}
+	//if (glm::length(direction) != 0 && glm::length(GetVelocity()->velocity) < m_maxSpeed) {
+	//	// v = v + direction.nor * a
+	//	// Got to take the normal of the direction so that player speed is calculated correctly on diagonals
+	//	GetVelocity()->velocity += glm::normalize(direction) * m_acceleration * ts.GetSeconds();
+	//} else {
+	//	// v = v + -v * deceleration
+	//	GetVelocity()->velocity += -GetVelocity()->velocity * m_deceleration * ts.GetSeconds();
+	//	if (glm::length(GetVelocity()->velocity) < 20.f)
+	//		GetVelocity()->velocity = { 0.f, 0.f, 0.f };
+	//}
 
-	// Stop all velocity when hit a wall
-	// Currently cannot wall slide
-	if (hitWall) {
-		GetVelocity()->velocity = { 0.f, 0.f, 0.f };
-	}
+	//// Stop all velocity when hit a wall
+	//// Currently cannot wall slide
+	//if (hitWall) {
+	//	GetVelocity()->velocity = { 0.f, 0.f, 0.f };
+	//}
 
-	if (glm::length(GetVelocity()->velocity) != 0)
-		GetVelocity()->velocity = glm::normalize(GetVelocity()->velocity) * glm::length(GetVelocity()->velocity);
+	//if (glm::length(GetVelocity()->velocity) != 0)
+	//	GetVelocity()->velocity = glm::normalize(GetVelocity()->velocity) * glm::length(GetVelocity()->velocity);
+
+	Engine::Entity::Move(direction, m_acceleration, m_maxSpeed, ts);
 
 	Engine::Entity::OnUpdate(ts);
 }
